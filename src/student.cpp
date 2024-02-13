06f07d959b9013830bece6866044ee04d24623af
t1m013y <t1m01y80m1n@gmail.com>
2024-02-13 19:56:04 +0300

    cjm

--- src/student.cpp
+++ src/student.cpp
@@ -1,5 +1,5 @@
 #include "student.h"
-
+     
 /**
  * @brief Инициализировать систему управления
  * @details Вызывется один раз перед вызовом
@@ -10,7 +10,14 @@ void controllerInit (Overlord &over)
     over.setSlider (SliderEnum::prog1, -12000, 12000);
     over.setSlider (SliderEnum::prog2, -10000, 10000);
 }
-
+   float step(uint32_t time, uint32_t period, float min_out, float max_out)
+{
+  if(time/period % 2)
+  {
+    return min_out;
+  }
+  return max_out;
+}
 /**
  * @brief Выполнить одну итерацию системы управления
  * @details Вызывается раз в 5мс
@@ -23,13 +30,13 @@ void controllerTick (Overlord &over)
     float motorAngle = over.getMotorTheta ();
     float motorVel = over.getMotorVel ();
 
-    float w0 = over.getSlider(SliderEnum::prog1) * 1.0 / 1000;
-    float e = w0 - motorVel;
+    // float w0 = over.getSlider(SliderEnum::prog1) * 1.0 / 1000;
+    // float e = w0 - motorVel;
 
-    static float I = 0;
-    static constexpr float Ki = 6.8;
-    static constexpr float Kp = 1;
-    static constexpr float Ts = 0.006;
+    // static float I = 0;
+    // static constexpr float Ki = 6.8;
+    // static constexpr float Kp = 1;
+    // static constexpr float Ts = 0.006;
 
     //float u = e * Kp;
     // float u = I;
@@ -37,24 +44,41 @@ void controllerTick (Overlord &over)
     // float dI = eKi * Ts;
     // I = I + dI; 
 
-    float eKp = e * Kp;
-    float u = I + eKp;
-    float umax = 12;
-    float eKi = eKp * Ki;
-    if(u == constrain(u, umax, -umax) ||
-        I * eKi < 0)
-    {
-        float dI = Ts * Ki;
-        I = I + dI;
-        
-    }       
+    // float eKp = e * Kp;
+    // float u = I + eKp;
+    // float umax = 12;
+    // float eKi = eKp * Ki;
+    // if(u == constrain(u, umax, -umax) ||
+    //     I * eKi < 0)
 
-    u = constrain(u, -umax, umax);
+    // {     
+    //     float dI = Ts * Ki;
+    //     I = I + dI;
+    //  }       
 
-    Serial.print(w0);
-    Serial.print(' ');
+    // u = constrain(u, -umax, umax);
+
+ 
+
+    uint32_t time = millis();
+
+    //  float w = motor.getSpeed();
+
+    float u = step(time, 2000, 0, 10); 
+
+    over.setMotorU(u);
+
+    Serial.print(time);
+    Serial.print(" ");
+    Serial.print(u);
+    Serial.print(" ");
     Serial.println(motorVel);
+
+    // Serial.print(w0);
+    // Serial.print(' ');
+    // Serial.println(motorVel);
 
     over.setMotorU (u);
 }
+ 
  
