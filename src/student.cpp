910d4cb5fe10232852d98b8d3b2fd637dc996a89
t1m013y <t1m01y80m1n@gmail.com>
2024-02-20 20:02:18 +0300

    Pi kof

--- src/student.cpp
+++ src/student.cpp
@@ -27,58 +27,42 @@ void controllerTick (Overlord &over)
     float setPoint = -over.getSetpoint ();
     float carX = -over.getCarX ();
     float carVel = -over.getCarVel ();
-    float motorAngle = over.getMotorTheta ();
-    float motorVel = over.getMotorVel ();
+    float motorAngle = over.getMotorTheta (); float setPoint = -over.getSetpoint ();
+    float motorvel = over.getMotorVel
 
-    // float w0 = over.getSlider(SliderEnum::prog1) * 1.0 / 1000;
-    // float e = w0 - motorVel;
+    over.setMotorU(0);
 
-    // static float I = 0;
-    // static constexpr float Ki = 6.8;
-    // static constexpr float Kp = 1;
-    // static constexpr float Ts = 0.006;
+    float w0 = over.getSlider(SliderEnum::prog1) * 1.0 / 1000;
 
-    //float u = e * Kp;
-    // float u = I;
-    // float eKi = e * Ki;
-    // float dI = eKi * Ts;
-    // I = I + dI; 
+    float e = w0 - motorVel;
 
-    // float eKp = e * Kp;
-    // float u = I + eKp;
-    // float umax = 12;
-    // float eKi = eKp * Ki;
-    // if(u == constrain(u, umax, -umax) ||
-    //     I * eKi < 0)
+    static float I = 0;
+    static constexpr float Ki = 4;
+    static constexpr float Kp = 19.84;
+    static constexpr float Ts = 0.006;
 
-    // {     
-    //     float dI = Ts * Ki;
-    //     I = I + dI;
-    //  }       
+    float eKp = e * Kp;
+    float u = I + eKp;
+    float uMax = 12;
+    float eKi = eKp * Ki;
+    if(u == constrain(u, umax, -umax) ||
+        I * eKi < 0)
+        
+    {     
+        float dI = Ts * Ki;
+        I = I + dI;
+     }     
 
-    // u = constrain(u, -umax, umax);
-
- 
-
-    uint32_t time = millis();
-
-    //  float w = motor.getSpeed();
-
-    float u = step(time, 2000, 0, 10); 
+    u = constrain(u, -umax, umax);
 
     over.setMotorU(u);
 
-    Serial.print(time);
-    Serial.print(" ");
-    Serial.print(u);
-    Serial.print(" ");
+    Serial.print(w0);
+    Serial.print(' ');
     Serial.println(motorVel);
 
-    // Serial.print(w0);
-    // Serial.print(' ');
-    // Serial.println(motorVel);
-
     over.setMotorU (u);
+
 }
  
  
